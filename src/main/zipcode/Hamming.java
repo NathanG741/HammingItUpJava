1e3f0f86e7d7ee34aaa3a2d181d24c29005843dd
nathan <nathan.guardado@yahoo.com>
2023-12-09 22:04:44 -0500

    Finished

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
 package zipcode;
 
+import java.util.Objects;
+
 public class Hamming {
+    private String s;
+    private String s1;
+
+
     public Hamming(String s, String s1) {
+        if (!Objects.equals(s.length(), s1.length())) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            this.s = s;
+            this.s1 = s1;
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+        int count = 0;
+        for (int i=0; i < s1.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
